e143e6b91261b86ba79862e17abca13a6890bda0
victoria-mojica <vikimojicabmo@gmail.com>
2025-06-16 00:21:18 -0300

    Añadi let

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ console.log("El script se esta ejecutando")
 let nombre = prompt("Estas a punto de iniciar una misión espacial, para entrar a la nave debes ingresar tu nombre");
 alert(`Bienvenido a la nave espacial, Capitan ${nombre}. Nos esperan días de supervivencia. ¿Estas listo? Vamos a explorar la galaxia`);
 
+let oxigeno = 10;
+let energia = 10;
+let comida = 10;
+let dia = 1;
+let jugando = true;
+

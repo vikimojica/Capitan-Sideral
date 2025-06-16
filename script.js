78048d739c050934062de936cea9e3e20b16db50
victoria-mojica <vikimojicabmo@gmail.com>
2025-06-16 00:11:35 -0300

    Rearmando el codigo juego Capitan Sideral

---
+++ script.js
@@ -0,0 +1,5 @@
+//juego Capitan Sideral
+console.log("El script se esta ejecutando")
+let nombre = prompt("Estas a punto de iniciar una misión espacial, para entrar a la nave debes ingresar tu nombre");
+alert(`Bienvenido a la nave espacial, Capitan ${nombre}. Nos esperan días de supervivencia. ¿Estas listo? Vamos a explorar la galaxia`);
+
